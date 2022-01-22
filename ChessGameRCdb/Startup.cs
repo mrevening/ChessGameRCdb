60209682c9ceb119e2587dc2bea3b3455cd5cbf6
dwieczorek <dwieczorek@kpmg.pl>
2022-01-23 00:06:42 +0100

    -

--- ChessGameRCdb/Startup.cs
+++ ChessGameRCdb/Startup.cs
@@ -13,7 +13,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Http.Extensions;
 
 namespace ChessGame
 {
