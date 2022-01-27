343bfa2b9388825a4e239be8fcfcbf38d87c71ec
dwieczorek <dwieczorek@kpmg.pl>
2022-01-28 00:11:55 +0100

    fix

--- ChessGameRCdbTests/GameTests.cs
+++ ChessGameRCdbTests/GameTests.cs
@@ -11,20 +11,20 @@ namespace ChessGameTests
         [Fact]
         public void StandardSetup_InputBoardHasAllFigures()
         {
-            var game = new ChessSession(new Board(setup.GetStandardSetup()));
+            var game = new ChessSession(new Board(setup.GetStandardSetup(), Color.White));
             Assert.Equal(32, game.InputBoard.Figures.Count());
         }
         [Fact]
         public void StartAndEndPointsAreTheSame_Error()
         {
-            var game = new ChessSession(new Board(setup.GetOnePawnSetup()));
+            var game = new ChessSession(new Board(setup.GetOnePawnSetup(), Color.White));
             void action() => game.PlayersAction(Color.White, new Coordinate(Column.D, Row.Two), new Coordinate(Column.D, Row.Two));
             Assert.Throws<StartAndEndPointAreTheSameException>(action);
         }
         [Fact]
         public void StartPoint_NotPlayersFigure_Error()
         {
-            var game = new ChessSession(new Board(setup.GetStandardSetup()));
+            var game = new ChessSession(new Board(setup.GetStandardSetup(), Color.White));
             void action() => game.PlayersAction(Color.White, new Coordinate(Column.D, Row.Seven), new Coordinate(Column.D, Row.Six));
             Assert.Throws<NotPlayersFigureException>(action);
         }
