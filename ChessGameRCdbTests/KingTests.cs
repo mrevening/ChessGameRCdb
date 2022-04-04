0582a779d803884640e4745ca6443ecc64d594e5
dwieczorek <dwieczorek@kpmg.pl>
2022-04-04 22:30:38 +0200

    add check test

--- ChessGameRCdbTests/KingTests.cs
+++ ChessGameRCdbTests/KingTests.cs
@@ -7,7 +7,7 @@ namespace ChessGameTests
 {
     public class KingTests
     {
-        public static IEnumerable<object[]> Data => new List<object[]> {
+        public static IEnumerable<object[]> Data1 => new List<object[]> {
             new object[] {
                 new King(Color.White, "A1"),
                 new Rook(Color.Black, "B8"),
@@ -16,22 +16,36 @@ namespace ChessGameTests
                     new MoveOption(ActionType.Move, new Log("A1", "A2"))
                 }
             }
-            //new object[] {
-            //    new King(Color.White, "E1"),
-            //    new Rook(Color.White, "A1"),
-            //    new List<MoveOption>()
-            //    {
-            //        new MoveOption(ActionType.Castle, new Log("A1", "A2", new List<LogComplexMove>() { new LogComplexMove(new CO) }))
-            //    }
-            //}
+            //complex move should be devided on castle, capture, promotion, 
         };
         [Theory]
-        [MemberData(nameof(Data))]
+        [MemberData(nameof(Data1))]
         public void FigureDeffersCheck_True(IFigure king, IFigure opponent, IEnumerable<MoveOption> expectedOptions)
         {
             var processor = new BoardProcessor(new Board(new List<IFigure>() { opponent, king })).CalculateInitBoard(king.Color);
             var actualMoves = processor.GetFigure(king.Coordinate).MoveOptions;
             Assert.Equal(expectedOptions, actualMoves);
         }
+
+        public static IEnumerable<object[]> Data2 => new List<object[]> {
+            new object[] {
+                new Pawn(Color.White, "D2"),
+                new King(Color.Black, "E5"),
+                new List<MoveOption>()
+                {
+                    new MoveOption(ActionType.Move, new Log("D2", "D3")),
+                    new MoveOption(ActionType.Check, new Log("D2", "D4"))
+                }
+            }
+        };
+        [Theory]
+        [MemberData(nameof(Data2))]
+        public void Check(IFigure opponent, IFigure king, IEnumerable<MoveOption> expectedOptions)
+        {
+            var processor = new BoardProcessor(new Board(new List<IFigure>() { opponent, king })).CalculateInitBoard(opponent.Color);
+            var actualMoves = processor.GetFigure(opponent.Coordinate).MoveOptions;
+            Assert.Equal(expectedOptions, actualMoves);
+        }
+        //complex move should be devided on castle, capture, promotion, 
     }
 }
